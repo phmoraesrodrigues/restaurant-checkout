d4c777bc1ccafc3ccd46bc21dfdb7d2832edafaa
Pedro Rodrigues <contact@pedrohmr.com>
2023-04-05 10:47:42 -0300

    Implement the get all categories endpoint

--- server/src/controllers/categories.ts
+++ server/src/controllers/categories.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+import { prismaClient } from "../../lib/prismadb";
 
-export const getAllCategories = (req: Request, res: Response) => {
-  return res.status(200).json({ status: "success", message: "All categories" });
+export const getAllCategories = async (req: Request, res: Response) => {
+  const categories = await prismaClient.category.findMany();
+
+  return res.status(200).json(categories);
 };
