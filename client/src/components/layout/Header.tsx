b8c2a78ac441c0a37c14772c1782fd35223d7c69
Pedro Rodrigues <contact@pedrohmr.com>
2023-04-05 17:17:35 -0300

    Add header elements

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,3 +1,43 @@
-const Header = () => <div>Header</div>;
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import AppBar from "@mui/material/AppBar";
+import Link from "@mui/material/Link";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const Header = () => (
+  <div>
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+    >
+      <Toolbar>
+        <RestaurantIcon />
+        <Typography variant="h6" color="inherit" noWrap sx={{ ml: 2, mr: 5 }}>
+          Restaurant Checkout
+        </Typography>
+        <nav>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="/"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Products
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="/checkout"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Cart
+          </Link>
+        </nav>
+      </Toolbar>
+    </AppBar>
+  </div>
+);
 
 export default Header;
