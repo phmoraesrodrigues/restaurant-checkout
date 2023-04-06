728961809c9c243acc5da37f4c11ea2eaa4c5333
Pedro Rodrigues <contact@pedrohmr.com>
2023-04-06 10:35:35 -0300

    Create model types

---
+++ client/src/common/model.ts
@@ -0,0 +1,12 @@
+export type Category = {
+  id: string;
+  name: string;
+  imageId: string;
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  imageId: string;
+  price: number;
+};
